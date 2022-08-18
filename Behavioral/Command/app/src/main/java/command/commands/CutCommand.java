b9c2cf276174b9ef2187cd9267c51fe6ea1e4dc9
Henrik Lange <h.l@outlook.de>
2022-08-18 12:32:56 +0200

    added back backup because it is necessary for undo

--- Behavioral/Command/app/src/main/java/command/commands/CutCommand.java
+++ Behavioral/Command/app/src/main/java/command/commands/CutCommand.java
@@ -12,6 +12,7 @@ public class CutCommand extends Command {
     public boolean execute() {
         if (editor.textField.getSelectedText().isEmpty()) return false;
 
+        backup();
         String source = editor.textField.getText();
         editor.clipboard = editor.textField.getSelectedText();
         editor.textField.setText(cutString(source));
